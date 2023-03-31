139b9b331460f32a041eef06f6c21b3cb9e5e27c
UstehKenny <carolina.kennedy@ingenieria.unam.edu>
2023-03-31 17:04:46 -0600

    Update estailus getting old

--- js/script.js
+++ js/script.js
@@ -12,10 +12,21 @@ function counter(){
     i++;
     if(i == 20){
         document.getElementById('age').innerHTML = i;
-        finalImg()
+        document.getElementById('flame').style.display = 'none';
+        document.getElementById('blinking-glow').style.display = 'none';
+        document.getElementById('glow').style.display = 'none';
+        
+    } else if (i == 18){
+        document.getElementById('tailus3').style.display = 'inline';
+        document.getElementById('tailus2').style.display = 'none';
+        document.getElementById('age').innerHTML = i;
+    } else if (i == 6){
+        document.getElementById('tailus2').style.display = 'inline';
+        document.getElementById('tailus1').style.display = 'none';
+        document.getElementById('age').innerHTML = i;
     } else if (i < 20){
         document.getElementById('age').innerHTML = i;
-    }
+    } 
     
 }
 function estasquad(){
