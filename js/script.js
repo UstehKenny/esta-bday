11a17b9986db8579afa1f6bf4564178b0d3e5d60
UstehKenny <carolina.kennedy@ingenieria.unam.edu>
2023-04-03 00:13:03 -0600

    Final functional update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var i = 0;
-
+var items = 0;
 document.addEventListener("keyup", function(event){
     if (event.keyCode === 27){ 
         document.getElementById('final').style.display = 'none';
@@ -7,6 +7,7 @@ document.addEventListener("keyup", function(event){
         document.getElementById('princesas-video').style.display = 'none';
         document.getElementById('arlett-video').style.display = 'none';
         document.getElementById('puzzle').style.display = 'none';
+        document.getElementById('win').style.display = 'none';
     } else return;
 });
 
@@ -30,20 +31,42 @@ function counter(){
         document.getElementById('tailus2').style.display = 'inline';
         document.getElementById('tailus1').style.display = 'none';
         document.getElementById('age').innerHTML = i;
+    } else if (i == 3){
+        document.getElementById('win').style.display = 'flex';
+        document.getElementById('win1').style.display = 'inline';
+        document.getElementById('age').innerHTML = i;
+    } else if (i == 9){
+        document.getElementById('win').style.display = 'flex';
+        document.getElementById('win2').style.display = 'inline';
+        document.getElementById('win1').style.display = 'none';
+        document.getElementById('age').innerHTML = i;
+    } else if (i == 14){
+        document.getElementById('win').style.display = 'flex';
+        document.getElementById('win3').style.display = 'inline';
+        document.getElementById('win2').style.display = 'none';
+        document.getElementById('age').innerHTML = i;
+    } else if (i == 19){
+        document.getElementById('win').style.display = 'flex';
+        document.getElementById('win4').style.display = 'inline';
+        document.getElementById('win3').style.display = 'none';
+        document.getElementById('age').innerHTML = i;
     } else if (i < 20){
         document.getElementById('age').innerHTML = i;
     } 
     
 }
 function estasquad(){
+    items++;
     document.getElementById('estasquad-video').style.display = 'flex';
 }
 
 function princesas(){
+    items++;
     document.getElementById('princesas-video').style.display = 'flex';
 }
 
 function arlett(){
+    items++;
     document.getElementById('arlett-video').style.display = 'flex';
 }
 
@@ -52,12 +75,16 @@ function finalImg(){
 }
 
 function puzzle(){
+    items++;
     document.getElementById('puzzle').style.display = 'flex';
 }
 function closeScreen(){
     document.getElementById('puzzle').style.display = 'none';
     document.getElementById('posterMom').style.display = 'none';
+    document.getElementById('win').style.display = 'none';
+    document.getElementById('final').style.display = 'none';
 }
 function posterMom(){
+    items++;
     document.getElementById('posterMom').style.display = 'flex';
 }
